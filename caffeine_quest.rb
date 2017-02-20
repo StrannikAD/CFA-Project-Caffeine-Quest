4011ea366c89c55255cfd95b781312fb48d5d5e9
Hannah <thompson.h@gmail.com>
2017-02-21 09:29:28 +1100

    setting up classes

---
+++ caffeine_quest.rb
@@ -0,0 +1,22 @@
+class User
+  def initialize(name)
+    @name = name
+  end
+
+  attr_accessor :name
+end
+
+class Room
+  def initialize()
+  end
+end
+
+class Question
+  def initialize(question, multiple_choice)
+    @question = question
+    @answer = ""
+    @multiple_choice = []
+end
+
+
+question1 = ()
