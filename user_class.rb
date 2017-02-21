0ead45ef34e4c36c902568b53521697af98a5209
AJ <advait.ju@gmail.com>
2017-02-21 17:12:12 +1100

    Modified user_class with methods to move into nexxt room and when restarting the game. Removed name attr_accessor as it is only set once.

--- user_class.rb
+++ user_class.rb
@@ -1,13 +1,20 @@
 class User
-  def initialize(name)
+  def initialize(name, current_room, hints)
     @name = name
+    @starting_room = current_room
+    @room = current_room
   end
 
-  attr_accessor :name
-
-  def deplete_caffeine
+  def next_room(room)
+    if room == @room
+      false
+    end
+    @room = room
+    true
   end
 
-  def change_location
+  def restart
+    @room = starting_room
   end
+
 end
