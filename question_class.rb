e120782b634e776e905a5e7f5cdf3042b5a7a0fb
AJ <advait.ju@gmail.com>
2017-02-21 17:35:21 +1100

    Added hint that is revealed upon getting a question right.

--- question_class.rb
+++ question_class.rb
@@ -1,8 +1,13 @@
 class Question
-  def initialize(question_text, multiple_choice, answer)
+  def initialize(question_text, multiple_choice, answer, hint)
     @question_text = question_text
     @multiple_choice = multiple_choice
     @answer = answer
+    @hint = hint
+  end
+
+  def hint
+    @hint
   end
 
   def multiple_choice
