f9e8f985c6ab0d925e51a96a8958e65087623504
Hannah <thompson.h@gmail.com>
2017-02-22 16:32:18 +1100

    added test for depleting caffeine levels

--- tests/test_user_class.rb
+++ tests/test_user_class.rb
@@ -3,11 +3,6 @@ require_relative "../player_class"
 
 class UserTest < Test::Unit::TestCase
 
-  # def test_player_displays_caffeine_level
-  #   player = Player.new("Player 1", "room 1")
-  #   assert_equal "Your current caffeine level is 100%", player.display_caffeine_level
-  # end
-
   def test_caffeine_level_depletes
     player = Player.new("Player 1", "room 1")
     player.deplete_caffeine
