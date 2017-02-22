4512f4a7cdd1de76f48922c2c27df5efa8ae63b9
Hannah <thompson.h@gmail.com>
2017-02-22 16:28:27 +1100

    Updated attributes and display method

--- room_class.rb
+++ room_class.rb
@@ -1,20 +1,19 @@
 class Room
-  def initialize(name, description, next_location)
+  def initialize(name, description, question, next_location)
     @name = name
     @description = description
-    @task = task
     @question = question
     @next_location = next_location
   end
 
-  attr_accessor :name, :description, :task
+  attr_accessor :name, :description, :task, :question
 
-  def display_task
-    puts "You need to do #{@task} before you can proceed..."
-  end
+  # def display_task
+  #   puts "You need to do #{@task} before you can proceed..."
+  # end
 
-  def display_question ##needs a display_question method in question class to work
-    question.display_question
+  def display_description
+    puts "You have entered #{name}. #{description}"
   end
 
 end
