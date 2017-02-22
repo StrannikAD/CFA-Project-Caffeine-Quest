73741ff800b54d899378e5125bc20382a7c842ae
AJ <advait.ju@gmail.com>
2017-02-22 16:06:29 +1100

    Added rooms as a property to school_class. Added intro_text, tutorial_text, success_text, failure_text and game wordmark text that are displayed at different stages.

--- school_class.rb
+++ school_class.rb
@@ -1,7 +1,21 @@
 class School
-  def initialize(player)
+  def initialize(player, rooms)
     @player = player
-    @rooms = []
+    @rooms = rooms
+
+    @intro_text = "Welcome to Caffeine Quest!"
+    @tutorial_text = "These are the commands you can enter: ..."
+    @success_text = "You were correct!"
+    @failure_text = "You were wrong! Try again."
+    @caffeine_quest_wordmark = %{
+                 ______      ________     _               ____                  __
+      ________  / ____/___ _/ __/ __/__  (_)___  ___     / __ \\__  _____  _____/ /_
+       ______  / /   / __ `/ /_/ /_/ _ \\/ / __ \\/ _ \\   / / / / / / / _ \\/ ___/ __/
+     _______  / /___/ /_/ / __/ __/  __/ / / / /  __/  / /_/ / /_/ /  __(__  ) /_
+              \\____/\\__,_/_/ /_/  \\___/_/_/ /_/\\___/   \\___\\_\\__,_/\\___/____/\\__/  }
+
+    start
+    player_prompt
   end
 
   def start
