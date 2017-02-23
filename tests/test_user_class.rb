9276e6f5486c0d8eeec5e39d30a3ccd791be388c
Hannah <thompson.h@gmail.com>
2017-02-23 13:50:51 +1100

    minor fixes, changed caffeine reduction amount

--- tests/test_user_class.rb
+++ tests/test_user_class.rb
@@ -6,6 +6,6 @@ class UserTest < Test::Unit::TestCase
   def test_caffeine_level_depletes
     player = Player.new("Player 1", "room 1")
     player.deplete_caffeine
-    assert_equal 90, player.caffeine
+    assert_equal 75, player.caffeine
   end
 end
