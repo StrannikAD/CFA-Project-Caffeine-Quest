12b0281a9f7b6d2a00c41de144d1b16a04a34292
AJ <advait.ju@gmail.com>
2017-02-23 12:20:08 +1100

    school_class updated with better messages and missing functionait.

--- school_class.rb
+++ school_class.rb
@@ -3,29 +3,74 @@ class School
     @player = player
     @rooms = rooms
 
-    @intro_text = "Welcome to Caffeine Quest!"
-    @tutorial_text = %{
-Caffeine Quest is a script adventure game - shoutout to the 90s, sans Y2K?
+    @intro_text = "Welcome to Caffeine Quest!\n\n"
+    @tutorial_text = %{Caffeine Quest is a script adventure game - shoutout to the 90s, sans Y2K?
 
 You interact with the game by typing in commands. Fingerless-gloves. Activate!
 
-Commands are case-insensitive. You can enter:
+Commands are case-insensitive:
 LOOK - Get information about current room.
 LEAVE - Answer question to leave current room and progress.
 H - View these tips again.
-RESTART - Low on Caffeine? Don't think you'll make it? Quit without quitting.
+RESTART - Another one. -DJ Khaled
 QUIT - If you must :(
 GODMODE - Anything, but this...
-    }
+
+ ______________________________________________________________________________________________
+|                                                                                              |
+|              HOW TO WIN:                                                                     |
+|              You start with a caffeine level of 100%. But you need more.                     |
+|              CoderFactory students have blocked all exits.                                   |
+|              Answer their riddles to get to the cafe that only does handle bars latte art.   |
+|              Wrong answers decaffeinate you! Do not get TOTALLY DECAFFEINATED !! ?? !        |
+|______________________________________________________________________________________________|
+}
     @invalid_input_text = "Command not found. Please try again."
-    @success_text = "You were correct!"
-    @failure_text = "You were wrong! Try again."
+    @success_text = "You were CORRECT!"
+    @failure_text = "You were WRONG! Try again."
+    @defeat_text = %{
+ ______________________________________________________________________________________________
+|                                                                                              |
+|               You did your best but it wasn't good enough.                                   |
+|               You've deen TOTALLY DECAFFEINATED! Try again to redeem yourself.               |
+|______________________________________________________________________________________________|
+    }
+    @victory_text = %{
+ ______________________________________________________________________________________________
+|                                                                                              |
+|               Nice job! You WON!                                                             |
+|               You have restored caffeine levels to 103%                                      |
+|                                                                                              |
+|               REWARDS:                                                                       |
+|               You are now eligible for a Caffeine Quest(TM) sticker!                         |
+|               -Lodge inquiries with Jamie.                                                   |
+|______________________________________________________________________________________________|
+    }
+
+    @game_end_text = %{
+  _____________________________________________________________________________________________
+|                                                                                              |
+|               Thank you for playing Caffeine Quest!                                          |
+|               May you always have a hipster cafe to recover in wherever you go.              |
+|                                                                                              |
+|               This game brought to you by the following legends:                             |
+|               StrannikAD                                                                     |
+|               hannahcancode                                                                  |
+|               advaitju                                                                       |
+|                                                                                              |
+|               https://github.com/advaitju/CFA-Project-Caffeine-Quest                         |
+|               \u2726 Follow + Star + Watch us on Github \u2726                                          |
+|______________________________________________________________________________________________|
+    }
     @caffeine_quest_wordmark = %{
-                 ______      ________     _               ____                  __
-      ________  / ____/___ _/ __/ __/__  (_)___  ___     / __ \\__  _____  _____/ /_
-       ______  / /   / __ `/ /_/ /_/ _ \\/ / __ \\/ _ \\   / / / / / / / _ \\/ ___/ __/
-     _______  / /___/ /_/ / __/ __/  __/ / / / /  __/  / /_/ / /_/ /  __(__  ) /_
-              \\____/\\__,_/_/ /_/  \\___/_/_/ /_/\\___/   \\___\\_\\__,_/\\___/____/\\__/  }
+ _______________________________________________________________________________________________
+|                 ______      ________     _               ____                  __             |
+|      ________  / ____/___ _/ __/ __/__  (_)___  ___     / __ \\__  _____  _____/ /_            |
+|       ______  / /   / __ `/ /_/ /_/ _ \\/ / __ \\/ _ \\   / / / / / / / _ \\/ ___/ __/            |
+|     _______  / /___/ /_/ / __/ __/  __/ / / / /  __/  / /_/ / /_/ /  __(__  ) /_              |
+|              \\____/\\__,_/_/ /_/  \\___/_/_/ /_/\\___/   \\___\\_\\__,_/\\___/____/\\__/              |
+|_______________________________________________________________________________________________|
+            }
 
     start
     player_prompt
@@ -44,9 +89,17 @@ GODMODE - Anything, but this...
 
   # Inifinite loop that runs till user enters 'quit'
   def player_prompt
-    loop do
+    while true
+      if false
+        puts @defeat_text
+        if !game_restart(:prompt)
+          break
+        end
+      end
+      puts ""
       print "Input ('h' for help) >> "
       input = gets.chomp.downcase
+      puts ""
 
       case input
       when "leave"
@@ -54,15 +107,16 @@ GODMODE - Anything, but this...
       when "look"
         player_command_look
       when "restart"
-        game_restart
+        game_restart(:no_prompt)
       when "h"
         puts @tutorial_text
+      when "quit"
+        break
       else
         puts @invalid_input_text
       end
-      break if input == "quit"
     end
-    puts "Thank you for playing Caffeine Quest!\nMay you always have a hipster cafe to recover in wherever you go."
+    puts @game_end_text
     puts @caffeine_quest_wordmark
   end
 
@@ -72,11 +126,20 @@ GODMODE - Anything, but this...
     @player.location.question.display_question
     print "Answer (enter A, B, C etc.) >> "
     input = gets.chomp.downcase
+    puts ""
     if @player.location.question.test_answer(input)
+      if @player.location == @rooms[-1]
+        puts @victory_text
+        game_restart(:prompt)
+      end
       puts @success_text
       @player.change_location(@player.location.next_location)
+      puts ""
+      player_entered_room
+    else
+      @player.deplete_caffeine
+      puts @failure_text
     end
-    puts @failure_text
   end
 
   ## LOOK
@@ -89,7 +152,22 @@ GODMODE - Anything, but this...
   ## ~ Cheats / Easter eggs
 
   # Game logic
-  def game_restart
+  def game_restart(prompt)
+    if prompt == :prompt
+      puts ""
+      print "Restart [y/n]? >> "
+      input = gets.chomp.downcase
+      puts ""
+
+      case input
+      when 'y' || 'yes'
+        game_restart
+      when 'n' || 'no'
+        return
+      else
+        @invalid_input_text
+      end
+    end
     start
     @player.restart(@rooms[0])
     player_entered_room
