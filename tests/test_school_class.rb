1816d28ec806f8d28751aa0e0ac76f5532f0e54b
Hannah <thompson.h@gmail.com>
2017-02-23 15:53:42 +1100

    two working tests per class

--- tests/test_school_class.rb
+++ tests/test_school_class.rb
@@ -1,6 +1,15 @@
 require "test/unit"
 require_relative "../school_class"
+require "paint"
 
 class SchoolTest < Test::Unit::TestCase
+  def test_class_initializes_correctly
+    school = School.new("Player", ["rooms"])
+    assert_equal "Player", school.player
+  end
 
+  def test_blink_prompt_outputs_a_coloured_string
+    school = School.new("Player", ["rooms"])
+    assert_equal "\e[5m>> \e[0m", school.blink_prompt
+  end
 end
