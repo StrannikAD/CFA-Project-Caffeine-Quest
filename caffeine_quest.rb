c246d5a05c17d40be74a1d44b4510382cecace59
Hannah <thompson.h@gmail.com>
2017-02-23 15:14:13 +1100

    added line breaks

--- caffeine_quest.rb
+++ caffeine_quest.rb
@@ -48,11 +48,11 @@ cafe_question = Question.new("Welcome to Cafe Hipst! You look exhausted, can I g
 
 #rooms need to be made backwards so they can feed into the next_location of their previous room
 cafe = Room.new("Cafe", "You did it!!\n\nYou will now receive a year's supply of take away coffee! j/k Reaching caffeine enlightenment is a reward in itself\n\nMay Java be with you!", cafe_question, nil)
-stairwell = Room.new("Stairwell", "The stairwell lights are eerily flickering on and off and was that a mouse that just ran across your foot?\n\nBut the end is in sight, caffeine awaits beyond this door!", question5, cafe)
+stairwell = Room.new("Stairwell", "The stairwell lights are eerily flickering on and off and was that a mouse \nthat just ran across your foot?\n\nBut the end is in sight, caffeine awaits beyond this door!", question5, cafe)
 printerroom = Room.new("Printer Room", "Disaster!\n\nThe printer has malfunctioned and is printing out pages at random.\n\nEscape!", question4, stairwell)
 kitchen = Room.new("Kitchen", "The kitchen is full to the brim with Year 9 students and there is no caffeine in sight.\n\nBeat a hasty retreat!", question3, printerroom)
-makerspace = Room.new("Maker Space", "There's a VR headset discarded on the floor - no doubt another unsuspecting student has fallen victim to the high beam jump!\n\nWhat sort of devilry is afoot?", question2, kitchen)
-classroom = Room.new("Classroom", "You have been working on your project for hours and are starting to get tired - time to get some caffeine.\n\nSuddenly the steady hum of computers is drowned out by a shriek in the adjoining room.\n\nPress \"LEAVE\' to investigate.", question1, makerspace)
+makerspace = Room.new("Maker Space", "There's a VR headset discarded on the floor\n - no doubt another unsuspecting student has fallen victim to the high beam jump!\n\nWhat sort of devilry is afoot?", question2, kitchen)
+classroom = Room.new("Classroom", "You have been working on your project for hours and are starting to get tired\n - time to get some caffeine.\n\nSuddenly the steady hum of computers is drowned out by a shriek in the adjoining room.\n\nPress \"LEAVE\' to investigate.", question1, makerspace)
 
 
 player = Player.new("Player One", classroom)
