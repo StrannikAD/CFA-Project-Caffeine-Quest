2f65ace57df9a094843f34083fb05c078c3856e5
StrannikAD <a.dimitrova.au@gmail.com>
2017-02-23 11:56:38 +1100

    Entered Room Descriptions

--- caffeine_quest.rb
+++ caffeine_quest.rb
@@ -45,13 +45,14 @@ cafe_question = Question.new("Welcome to Cafe Hipst! You look exhausted, can I g
     "d) Ethiopian cold drip",
     "e) I could murder a Nescafe Blend 43"], "e")
 
+
 #rooms need to be made backwards so they can feed into the next_location of their previous room
-cafe = Room.new("Cafe", "description", cafe_question, nil)
-stairwell = Room.new("Stair Well", "description", question5, cafe)
-printerroom = Room.new("Printer Room", "description", question4, stairwell)
-kitchen = Room.new("Kitchen", "description", question3, printerroom)
-makerspace = Room.new("Maker Space", "description", question2, kitchen)
-classroom = Room.new("Classroom", "description", question1, makerspace)
+cafe = Room.new("Cafe", "You did it!!\n\nYou will now receive a year's supply of take away coffee! j/k Reaching caffeine enlightenment is a reward in itself\n\nMay Java be with you!", cafe_question, nil)
+stairwell = Room.new("Stairwell", "The stairwell lights are eerily flickering on and off and was that a mouse that just ran across your foot?\n\nBut the end is in sight, caffeine awaits beyond this door!", question5, cafe)
+printerroom = Room.new("Printer Room", "Disaster!\n\nThe printer has malfunctioned and is printing out pages at random.\n\nEscape!", question4, stairwell)
+kitchen = Room.new("Kitchen", "The kitchen is full to the brim with Year 9 students and there is no caffeine in sight.\n\nBeat a hasty retreat!", question3, printerroom)
+makerspace = Room.new("Maker Space", "There's a VR headset discarded on the floor - no doubt another unsuspecting student has fallen victim to the high beam jump!\n\nWhat sort of devilry is afoot?", question2, kitchen)
+classroom = Room.new("Classroom", "You have been working on your project for hours and are starting to get tired - time to get some caffeine.\n\nSuddenly the steady hum of computers is drowned out by a shriek in the adjoining room.\n\nPress \"LEAVE\' to investigate.", question1, makerspace)
 
 
 player = Player.new("Player One", classroom)
